41ef2fdfe215ca777ce453a5e8991c0f1f9a0832
Emeka Emodi <emodiemeka@gmail.com>
2023-07-14 12:55:20 -0700

    Fix API status route

--- status_server/api/v1/app.py
+++ status_server/api/v1/app.py
@@ -2,8 +2,9 @@
 """
 Web server 
 """
-from views.index import status
+from .views.index import status
 from flask import Flask, jsonify, make_response
+import socket
 
 app = Flask(__name__)
 
@@ -12,7 +13,19 @@ def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
 
+# Register the status function as route.
+app.add_url_rule('/api/v1/status', view_func=status, methods=['GET'])
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
-    app.run(host="0.0.0.0", port=8080, threaded=True)
+    """python -m api.v1.app"""
+    port = 5000 # Default port
+    try:
+        # Attempt to create a socket connection if default port is already in use
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(("0.0.0.0", port))
+        sock.close()
+    except socket.error:
+        # If port is already in use, switch to an alternative port(e.g. 8080)
+        port = 8080
+
+    app.run(host="0.0.0.0", port=port, threaded=True)
--- status_server/api/v1/views/__init__.py
+++ status_server/api/v1/views/__init__.py
@@ -4,4 +4,4 @@
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from .index import *
+from .index import status
